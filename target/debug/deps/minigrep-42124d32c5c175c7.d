b592b29291f9fb1e3c5075f8b1a17ab18cf42175
ronaldkohhh <ronald9872@gmail.com>
2023-11-02 17:27:55 +0800

    updates

---
+++ target/debug/deps/minigrep-42124d32c5c175c7.d
@@ -0,0 +1,5 @@
+/Users/ronaldkoh/Desktop/rust-book/minigrep/target/debug/deps/minigrep-42124d32c5c175c7: src/lib.rs
+
+/Users/ronaldkoh/Desktop/rust-book/minigrep/target/debug/deps/minigrep-42124d32c5c175c7.d: src/lib.rs
+
+src/lib.rs:
---
+++ target/debug/deps/minigrep-88740f123022a38d.d
@@ -0,0 +1,5 @@
+/Users/ronaldkoh/Desktop/rust-book/minigrep/target/debug/deps/minigrep-88740f123022a38d: src/main.rs
+
+/Users/ronaldkoh/Desktop/rust-book/minigrep/target/debug/deps/minigrep-88740f123022a38d.d: src/main.rs
+
+src/main.rs:
